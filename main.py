81fcb33f9bd58e434224e71e7f4dce882ecb0397
mpicazocp <mpicazo@calpoly.edu>
2023-02-14 09:01:35 -0800

    main.py additions NOT DONE

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from imaplib import _CommandResults
 import multiprocessing
 from multiprocessing.dummy import Process
 from multiprocessing import Pipe
+from xml.etree.ElementTree import tostring
+import gps_file as gps
 
 from Communications.communications.parent import parent_proc
 
@@ -11,17 +13,29 @@ def wait_for_rover(args):
     #wait for a recv from the rover suggesting its done
     UAV.recv()
 
+def send_location(gps_coor):
+    comms.send(gps_coor)
+
+def calibrate_coordinates(gps_coor):
+    #do some math!!!
+    latitude = gps_coor[0]
+    longitude = gps_coor[1]
+    gps_string = tostring(latitude) + " " + tostring(longitude)
+    send_location(gps_string)
+    
+    
 function_set = {
-    #"EXECUTION": wait_for_rover,
     "EX_DONE": comms.send("PICKLE")
 }
 
 def main():
-    
-
 
     communications = Process(target=parent_proc, args=("192.168.4.3",7777, "192.168.4.10", 7676, function_set))
     communications.start()
+    #if red square is found: 
+        #gps_coor = gps.gps_setup()
+        #calibrate_coordinates(gps_coor)
+        
 
 
     communications.join()
